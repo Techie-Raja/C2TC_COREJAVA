e813b5c6fd9787042e6cc7d5f1184389a6b08085
karth <karth@KarthikRaja>
2024-10-27 21:35:22 +0530

    INITIAL COMMIT

---
+++ C2TC_KARTHIKRAJAB/src/com/tns/ifetdayone/IdentifersDemo.java
@@ -0,0 +1,10 @@
+package com.tns.ifetdayone;
+
+public class IdentifersDemo {
+	public static void main(String[] args) {
+		int $number = 20;
+		System.out.println("value of the number variable is : "+ $number);
+		String studentName="Karthik Raja B";
+		System.out.println("value is : "+ studentName);
+	}
+}
