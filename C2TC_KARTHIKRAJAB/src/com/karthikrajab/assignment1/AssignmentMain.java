2be2250892a3f9a557c4c1799b251a6b244d9cc7
karth <karth@KarthikRaja>
2024-12-14 13:33:44 +0530

    ASSIGNMENT 1

---
+++ C2TC_KARTHIKRAJAB/src/com/karthikrajab/assignment1/AssignmentMain.java
@@ -0,0 +1,9 @@
+package com.karthikrajab.assignment1;
+public class AssignmentMain {
+public static void main(String[] args) {
+Manager manager = new Manager("John", 101, 90000, "Sales");
+Developer developer = new Developer("Alice", 102, 80000, "Java");
+EmployeeUtilities.printEmployeeDetails(manager);
+EmployeeUtilities.printEmployeeDetails(developer);
+}
+}
